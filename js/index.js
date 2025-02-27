85d2babdb785c000c7f63fda9646f052a72625a2
rladmstn <oz33177351@gmail.com>
2025-02-27 14:08:21 +0900

    feat: 요소 수정 및 사진 추가

--- js/index.js
+++ js/index.js
@@ -1,19 +1,34 @@
-console.log(
-  "%c" +
-    " __      __  ______   __  __   ______     " +
-    "\n" +
-    "/\\ \\  __/\\ \\ /\\__  _\\ /\\ \\ /\\ \\ /\\__  _\\    " +
-    "\n" +
-    "\\ \\ \\/\\ \\ \\ \\/_\\/\\ \\/ \\ \\ \\/'/'\\/ _/\\ \\/    " +
-    "\n" +
-    " \\ \\ \\ \\ \\ \\ \\ \\ \\ \\  \\ \\ , <    \\ \\ \\    " +
-    "\n" +
-    "  \\ \\ \\_/ \\_\\ \\ \\_\\ \\__\\ \\ \\\\`\\   \\_\\ \\__ " +
-    "\n" +
-    "   \\ `\\___x___/ /\\_____\\\\ \\_\\ \\_\\ /\\_____\\ " +
-    "\n" +
-    "    '/__//__/  /_____/ \\/_/\\/_/ /_____/",
-  "color: #d81b60; font-size: 16px; font-weight: bold;"
-);
+// 댓글 등록 버튼 클릭 시 실행
+document.getElementById('button').addEventListener('click', function() {
+  // 댓글 입력값 가져오기
+  const commentText = document.getElementById('commentText').value.trim();
 
-console.log("알맞은 스크립트를 작성하세요");
+  // 댓글 입력값이 비어 있지 않으면
+  if (commentText !== '') {
+    // 댓글 목록을 가져옴
+    const commentList = document.getElementById('commentList');
+
+    // 새로운 댓글 항목 생성
+    const newComment = document.createElement('li');
+    newComment.classList.add('comment-item');
+    
+    // 댓글 내용 추가
+    newComment.innerHTML = `
+      <div class="comment-author">방문자</div>
+      <div class="comment-content">${commentText}</div>
+    `;
+
+    // 댓글 목록에 새 댓글 추가
+    commentList.appendChild(newComment);
+
+    // 댓글 입력란 비우기
+    document.getElementById('commentText').value = '';
+  } else {
+    alert('댓글을 입력해주세요.');
+  }
+});
+
+// 댓글 취소 버튼 클릭 시 입력란 초기화
+document.getElementById('reset').addEventListener('click', function() {
+  document.getElementById('commentText').value = '';
+});
